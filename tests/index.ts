30aecdc360d2e8471464c48a8c9c8a894d49564c
Mzaza <mzazakeith@gmail.com>
2025-07-07 01:03:03 +0300

    Published branch "develop"

--- tests/index.ts
+++
@@ -1,8 +0,0 @@
-import { describe } from 'manten';
-
-describe('aicommits', ({ runTestSuite }) => {
-	runTestSuite(import('./specs/cli/index.js'));
-	runTestSuite(import('./specs/gemini/index.js'));
-	runTestSuite(import('./specs/config.js'));
-	runTestSuite(import('./specs/git-hook.js'));
-});
